f4846370a6704fefa1ed10eea7bb8921157d97bc
benjamin <benjamin.erman@hotmail.com>
2023-05-24 17:39:27 +0200

    first commit

---
+++ src/views/AboutView.vue
@@ -0,0 +1,31 @@
+<template>
+  <v-app id="inspire">
+    <v-app-bar>
+      <v-spacer></v-spacer>
+      <v-tabs centered color="grey-darken-2">
+        <v-tab v-for="link in links" :key="link">
+          {{ link }}
+        </v-tab>
+      </v-tabs>
+      <v-spacer></v-spacer>
+    </v-app-bar>
+
+    <v-main>
+      <v-container>
+        <v-row>
+          <v-col v-for="n in 24" :key="n" cols="4">
+            <v-card height="200"></v-card>
+          </v-col>
+        </v-row>
+      </v-container>
+    </v-main>
+  </v-app>
+</template>
+
+<script scoped>
+export default {
+  data: () => ({
+    links: ["Home", "About"],
+  }),
+};
+</script>
